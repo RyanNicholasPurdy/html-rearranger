09c1f872885bef73d5a18638217a67d979e5003d
Ryan Purdy <Ryanpurdy@Ryans-MacBook-Pro-2.local>
2016-03-17 23:18:04 -0400

    Fixed market issue

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-var $h2Above = $('<h2>');
-var $h2Below = $('<h2>');
-var $ulAbove = $('<ul>');
-var $ulBelow = $('<ul>');
+var $h1above = $('<h1>');
+var $h1below = $('<h1>');
+var $ulabove = $('<ul>');
+var $ulbelow = $('<ul>');
+
+$h1above.html('Below ground veggies');
+$h1below.html('Above ground veggies');
+
+$('body').append($h1above);
+$('body').append($ulabove);
+
+$('body').append($h1below);
+$('body').append($ulbelow);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('above')) {
+    $ulabove.append($(this));
+  } else {
+    $ulbelow.append($(this));
+  }
+});
